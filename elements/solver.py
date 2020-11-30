ca47842cae0dd7e92d90de700b1b16ac1e481097
Bartosz Wróbel <bartosz.wrobel.yt@gmail.com>
2020-11-30 19:11:28 +0100

    algorithm restructurization part1

--- elements/solver.py
+++ elements/solver.py
@@ -1,13 +1,126 @@
-import random
 import numpy as np
+import random
+import copy
+from typing import *
 
 
 class Solver:
-    def __init__(self):
-        # self.target = target
-        # self.target_error = target_error
-        # self.n_particles = n_particles
-        self.particles = []
-        self.gbest_value = float('inf')
-        self.gbest_position = np.array(
-            [random.random()*50, random.random()*50])
+
+    def __init__(self, num_swarm: int, num_iterations: int, locations: List):
+        self.num_swarm = num_swarm
+        self.num_iterations = num_iterations
+        self.locations = locations
+        self.gBest = []
+        self.gBest_cost = 0
+
+    def swap(self, lst, n, m):
+        buff = lst[n-1]
+        lst[n-1] = lst[m-1]
+        lst[m-1] = buff
+        return lst
+
+    def diff(self, a, b):
+        list_of_diff = []
+        b_buff = b[:]
+        if len(a) != len(b):
+            print("Blad! Różne długości tablic")
+            return -1
+        for i in range(len(a)-1):
+            if a == b_buff:
+                break
+            else:
+                if a[i] != b_buff[i]:
+                    ind = b_buff.index(a[i])
+                    if ind == -1:
+                        return -1
+                    list_of_diff.append([i+1, ind])
+                    self.swap(b_buff, i+1, ind)
+        return list_of_diff
+
+    def calculate_part_cost(self, part, lst_of_points):
+        route = 0
+        for i in range(len(part)):
+            if i == 0:
+                route += np.sqrt(np.power(lst_of_points[part[i]-1][0], 2) +
+                                 np.power(lst_of_points[part[i]-1][1], 2))
+            if i == len(part) - 1:
+                route += np.sqrt(np.power(lst_of_points[part[i]-1][0], 2) +
+                                 np.power(lst_of_points[part[i]-1][1], 2))
+            else:
+                route += np.sqrt(np.power(lst_of_points[part[i]-1][0] - lst_of_points[part[i+1]-1][0], 2) +
+                                 np.power(lst_of_points[part[i]-1][1] - lst_of_points[part[i+1]-1][1], 2))
+        return route
+
+    # pkt poczatkowy to [0,0]
+
+    def calculate_full_cost(self, lst_of_part, lst_of_points):
+        lst_of_cost = []
+        for i in range(len(lst_of_part)):
+            cal = self.calculate_part_cost(lst_of_part[i], lst_of_points)
+            lst_of_cost.append(cal)
+        return lst_of_cost
+
+    def solve(self):             # n -> liczebność stada  m -> liczba iteracji   l -> liczba klientow
+        list_of_particle = []
+        dict_of_vel = {}
+        list_of_pBest = []
+
+        l = len(self.locations)
+
+        self.gBest = 0
+
+        for i in range(self.num_swarm):
+            list_of_particle.append(random.sample(range(1, l+1), k=l))
+            dict_of_vel[i] = [(random.sample(range(1, l+1), k=2))]
+
+        list_of_pBest = copy.deepcopy(list_of_particle)
+        lst_of_pBest_cost = self.calculate_full_cost(
+            list_of_pBest, self.locations)
+        self.gBest_cost = min(lst_of_pBest_cost)
+        index_gBest = lst_of_pBest_cost.index(self.gBest_cost)
+        self.gBest = list_of_pBest[index_gBest]
+
+        check = False
+        for i in range(self.num_iterations):
+            for it in range(self.num_swarm):
+                diff1 = self.diff(list_of_particle[it], list_of_pBest[it])
+                diff2 = self.diff(list_of_particle[it], self.gBest)
+                if diff1:
+                    for p1 in range(len(diff1)):
+                        for c1 in range(len(dict_of_vel[it])):
+                            check = False
+                            if dict_of_vel[it][c1]:
+                                if dict_of_vel[it][c1][0] == diff1[p1][0] and dict_of_vel[it][c1][1] == diff1[p1][1]:
+                                    dict_of_vel[it][c1].clear()
+                                    check = True
+                                elif dict_of_vel[it][c1][0] == diff1[p1][1] and dict_of_vel[it][c1][1] == diff1[p1][0]:
+                                    dict_of_vel[it][c1].clear()
+                                    check = True
+                        if not check:
+                            dict_of_vel[it].append(diff1[p1])
+                if diff2:
+                    for p2 in range(len(diff2)):
+                        check = False
+                        for c2 in range(len(dict_of_vel[it])):
+                            if dict_of_vel[it][c2]:
+                                if dict_of_vel[it][c2][0] == diff2[p2][0] and dict_of_vel[it][c2][1] == diff2[p2][1]:
+                                    del dict_of_vel[it][c2]
+                                    check = True
+                                    break
+                                elif dict_of_vel[it][c2][0] == diff2[p2][1] and dict_of_vel[it][c2][1] == diff2[p2][0]:
+                                    del dict_of_vel[it][c2]
+                                    check = True
+                                    break
+                        if not check:
+                            dict_of_vel[it].append(diff2[p2])
+                for el in range(len(dict_of_vel[it])):
+                    if dict_of_vel[it][el]:
+                        list_of_particle[it] = self.swap(
+                            list_of_particle[it], dict_of_vel[it][el][0], dict_of_vel[it][el][1])
+            costs = self.calculate_full_cost(list_of_particle, self.locations)
+            for it in range(self.num_swarm):
+                if costs[it] < lst_of_pBest_cost[it]:
+                    list_of_pBest[it] = list_of_particle[it]
+                    lst_of_pBest_cost[it] = costs[it]
+                if costs[it] < self.gBest_cost:
+                    self.gBest = list_of_particle[it]
---
+++ ipds-pso.py
@@ -0,0 +1,17 @@
+from elements.particle import Particle
+from elements.solver import Solver
+from elements.graph import Graph
+from random import random
+
+
+def main():
+    lst_of_points = [[49.981862518527194, 19.948879677062486],
+                     [49.9818383674969, 19.948069653078814], [49.98329150894827, 19.946237372007527], [49.98333981650393, 19.946972318011984]]
+    s = Solver(100, 1000, lst_of_points)
+    s.solve()
+    print(s.gBest)
+    print(s.gBest_cost)
+
+
+if __name__ == "__main__":
+    main()

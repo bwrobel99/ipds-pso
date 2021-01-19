fb35e07a9f90e8cf0d224084d3910c552119efe9
Bartosz Wróbel <49716756+bwrobel99@users.noreply.github.com>
2021-01-19 09:29:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ def calculate():
     data = json.loads(request.data)
     lst_of_points = data['places'][1:]
     restaurant_loc = data['places'][0]
+    fuel_level = data['petrol']
     petrol_locations = [[50.06, 19.92]]
-    s = Solver(25, 25, 5.5, lst_of_points, restaurant_loc)
+    s = Solver(25, 25, fuel_level, lst_of_points, restaurant_loc)
     delivery_time, pizza_temperature, used_fuel = s.solve()
     return json.dumps({"delivery_time": delivery_time, "pizza_temperature": pizza_temperature, 
     "used_fuel": used_fuel, "order": s.gBest, "km": np.around(s.gBest_cost,2), "petrol_locations": s.petrol_locations})
